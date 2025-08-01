c11c1ba838c81bd084b6de9184cad773be406f27
arpan7sarkar <arpansakar@gmail.com>
2025-08-02 01:32:41 +0530

    created functions to make the code more redable

--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,47 @@
+
 const itemsContainerElement = document.querySelector(".items-container");
-let innerhtm='';
-items.forEach(item=>{
-    innerhtm+=`<div class="item-container">
-                <img src="${item.item_image}" alt="item image">
-                <div class="rating">
-                    ${item.rating.star}⭐|${item.rating.no_of_review}
-                </div>
-                <div class="company">
-                   ${item.company_name}
-                </div>
-                <div class="item-name">${item.item_name}</div>
-                <div class="price">
-                    <span class="current-price">Rs ${item.price.current_price}</span><span class="original-price">Rs ${item.price.original_price}</span><span class="discount">(${item.price.discount}% off)</span>
-                </div>
-                <button class="btn-add-bag">Add to Bag</button>
-            </div>`
-})
-itemsContainerElement.innerHTML=innerhtm;
+let bagItems=[];
+onload();
+function onload(){
+    let bagItemsStr=localStorage.getItem('bagItems');
+    bagItems=bagItemsStr?JSON.parse(bagItemsStr):[];
+    displayItemsHomePage();
+    displayBagIcon();
+
+}
+function displayBagIcon(){
+    let bagItemCountElem=document.querySelector('.bag-item-count');
+    if(bag.items.length>0){
+        bagItemCountElem.style.visibility='visible';
+        bagItemCountElem.innerHTML=bagItems.length;
+    }else{
+        bagItemCountElem.style.visibility='hidden';
+        
+    }
+}
+function displayItemsHomePage(){
+    let innerhtm='';
+    items.forEach(item=>{
+        innerhtm+=`<div class="item-container">
+                    <img src="${item.item_image}" alt="item image">
+                    <div class="rating">
+                        ${item.rating.star}⭐|${item.rating.no_of_review}
+                    </div>
+                    <div class="company">
+                       ${item.company_name}
+                    </div>
+                    <div class="item-name">${item.item_name}</div>
+                    <div class="price">
+                        <span class="current-price">Rs ${item.price.current_price}</span><span class="original-price">Rs ${item.price.original_price}</span><span class="discount">(${item.price.discount}% off)</span>
+                    </div>
+                    <button class="btn-add-bag" onclick="addToBag(${item.id})">Add to Bag</button>
+                </div>`
+    })
+    itemsContainerElement.innerHTML=innerhtm;
+}
+function addToBag(itemId){
+    bagItems.push(itemId);
+    localStorage.setItem('bagItems',JSON.stringify(bagItems));
+    displayBagIcon();
+}
 
